3583077cf705b851355290773a2897ccdc13179b
Mayank <82028762+saurabhMayank@users.noreply.github.com>
2023-12-29 08:14:35 +0000

    added files of depdendency injection and dependency inversion

---
+++ solid_udemy_course/srp/dep_injectionvsdep_inversion.py
@@ -0,0 +1,23 @@
+"""
+Dependency Injection
+"""
+
+"""
+Lets first understand in very simple terms what is dependency Injection
+
+=> You are making a sandwich -> U need bread, cheese, ham
+=> Your core job is to make the sandwich -> But you need the depedencies bread, cheese, ham -> To make the sandwich
+
+IF USING DEPENDENCY INJECTION CONCEPT
+=> One way is to get these depedencies from a 3rd party
+Lets do you do bigbasket -> And the guy delivers you the depdencies
+
+i) You can now solely focus on making the sandwich
+ii) You are solely focussed on preparing for the sandwich 
+-> Setting the sandwich maker, getting the sauces & veggies ready
+iii) When the big basket guy comes with depdencies of bread, ham and cheese 
+-> You inject those dependencies -> In your sandwich
+
+IF NOT USING DEPENDENCY INJECTION CONCEPT
+=> Then I have to myself take care of getting bread, ham and cheese
+"""
