03988f0622fdd1398a403060589e7e0c654573b2
Mayank <82028762+saurabhMayank@users.noreply.github.com>
2024-03-31 15:46:55 +0530

    Update lsp.py

--- LSP/lsp.py
+++ LSP/lsp.py
@@ -1,1 +1,8 @@
 # LSP -> Liskov Substitution Principle
+# Child class can take care of all the responsibilities -> that parent class can do
+# In that case -> Child class Instance -> Can be substituted in place of parent class instance -> In real world program
+
+# Why its Important ?
+# Let us understand that -> Breaking down to 1st Principle -> Why do we need LSP
+# Break it down to the most fundamental truth and then build from ground up there on
+
