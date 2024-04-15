3e8c769984c5b7edde63d2897c694062a39f1dfb
Mayank <82028762+saurabhMayank@users.noreply.github.com>
2024-04-15 14:53:43 +0530

    Update lsp.py

--- LSP/lsp.py
+++ LSP/lsp.py
@@ -6,3 +6,16 @@
 # Let us understand that -> Breaking down to 1st Principle -> Why do we need LSP
 # Break it down to the most fundamental truth and then build from ground up there on
 
+# classes as reusable building blocks for your code. A parent class defines a general blueprint,
+# and subclasses inherit this blueprint with potentially added functionalities or specializations
+
+# Subclasses are inherited from parent class -> so it has all the property of parent classes
+# it should be able to execute all the functionalities of the parent and some added functionalities
+
+# ex -> Parent class: Shape
+# Subclass: Square
+
+# Parent class Shape has calc_area() functionality to calculate the area of a shape
+# when extended the cacl_area() function should calculate area in square class also
+
+# If child class is not able to execute all the functionalities of the parent class -> means inheritance is not proper
